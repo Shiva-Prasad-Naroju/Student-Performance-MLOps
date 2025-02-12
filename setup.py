ab3bbbde20e3f7caaf34d92b8bd3367fadfd447d
Shiva Prasad Naroju <shivanaroju26@gmail.com>
2025-02-12 23:19:37 +0530

    Update setup.py
    Description added to the setup.py

--- setup.py
+++ setup.py
@@ -5,23 +5,30 @@ from typing import List
 HYPHEN_E_DOT = '-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
-    this function will return the list of requirements
+    This function reads a requirements.txt file and returns a list of dependencies.
+    It also removes '-e .' if present, as it is used for editable installs and not an actual package dependency.
     '''
     requirements = []
+
+    # Open and read the requirements file
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
+        
+        # Remove newline characters from each requirement
         requirements = [req.replace("\n","") for req in requirements]
-
+        
+        # Remove '-e .' if it exists, since it's only needed for local development
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
+            
     return requirements
 
-
+# Setup configuration for the ML project package
 setup(
     name = 'MLproject',
-    version = '0.0.1',
+    version = '0.0.1', # Version of the package
     author = 'Shiva Prasad Naroju',
-    author_email = 'shivanaroju26@gmail.com',
-    packages = find_packages(),
-    install_requires = get_requirements('requirements.txt')
+    author_email = 'shivanaroju26@gmail.com', # Contact email of the author
+    packages = find_packages(), # Automatically discovers all packages in the project
+    install_requires = get_requirements('requirements.txt') # Fetch dependencies from requirements.txt
 )
