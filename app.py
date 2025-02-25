743d42b521bb1acc89af0b738e51ff107242e96f
Shiva Prasad Naroju <shivanaroju26@gmail.com>
2025-02-25 22:31:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
-app=application
+app = application
+
 ## Route for a home page
 @app.route('/')
 def index():
