e13ae4d01c662989dc13f6f90ed6ed215f9e07c2
Shiva-Prasad-Naroju <shivanaroju26@gmail.com>
2025-03-10 00:23:58 +0530

    update

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         
+        
         # Remove newline characters from each requirement
         requirements = [req.replace("\n","") for req in requirements]
         
