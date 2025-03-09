24f873b1d144042bdda83b6f30232d74593be706
Shiva-Prasad-Naroju <shivanaroju26@gmail.com>
2025-03-10 00:09:25 +0530

    updated

--- app.py
+++ app.py
@@ -21,13 +21,13 @@ def predict_datapoint():
         try:
             # Fix input field mapping
             data = CustomData(
-                gender=request.form.get('gender'),
-                race_ethnicity=request.form.get('ethnicity'),
-                parental_level_of_education=request.form.get('parental_level_of_education'),
-                lunch=request.form.get('lunch'),
-                test_preparation_course=request.form.get('test_preparation_course'),
-                reading_score=float(request.form.get('reading_score')),
-                writing_score=float(request.form.get('writing_score'))
+                gender = request.form.get('gender'),
+                race_ethnicity = request.form.get('ethnicity'),
+                parental_level_of_education = request.form.get('parental_level_of_education'),
+                lunch = request.form.get('lunch'),
+                test_preparation_course = request.form.get('test_preparation_course'),
+                reading_score = float(request.form.get('reading_score')),
+                writing_score = float(request.form.get('writing_score'))
             )
 
             # Convert to DataFrame
