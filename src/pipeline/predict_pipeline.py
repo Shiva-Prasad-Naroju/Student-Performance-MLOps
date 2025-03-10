ef2196bdba9a0d3a9b1bb7c97e17c667913836bb
Shiva Prasad Naroju <shivanaroju26@gmail.com>
2025-03-10 09:39:58 +0530

    Update predict_pipeline.py

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -35,8 +35,8 @@ class PredictPipeline:
 
 class CustomData:
     def __init__(  self,
-        gender: str,
-        race_ethnicity: str,
+        gender: str,        
+        race_ethnicity: str,         
         parental_level_of_education,
         lunch: str,
         test_preparation_course: str,
@@ -61,11 +61,17 @@ class CustomData:
         try:
             custom_data_input_dict = {
                 "gender": [self.gender],
+                
                 "race_ethnicity": [self.race_ethnicity],
+                
                 "parental_level_of_education": [self.parental_level_of_education],
+                
                 "lunch": [self.lunch],
+                
                 "test_preparation_course": [self.test_preparation_course],
+                
                 "reading_score": [self.reading_score],
+                
                 "writing_score": [self.writing_score],
             }
 
